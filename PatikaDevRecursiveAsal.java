7050ed0e3c60c9ee3e75db8a1af735924414e78f
Akin Buzkus <83575077+Akinbuzkus@users.noreply.github.com>
2022-12-05 19:40:38 +0300

    Add files via upload

---
+++ PatikaDevRecursiveAsal.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+public class PatikaDevRecursiveAsal {
+    
+    
+     static boolean isPrime(int n, int i)
+    {
+        // Base cases
+        if (n <= 2) 
+           return (n == 2) ? true : false; 
+       if (n % i == 0) 
+           return false; 
+       if (i * i > n)
+
+            return true;
+        // Check for next divisor
+        return isPrime(n, i + 1);
+    }
+
+  // Driver program to test above function
+  public static void main(String[] args)
+  {  
+    int n = 22;
+    if (isPrime(n, 2))
+      System.out.println("Yes it's a Prime");
+   else
+     System.out.println("Not a Prime");
+   }
+ }
